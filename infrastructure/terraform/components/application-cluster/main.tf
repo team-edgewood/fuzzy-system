4cd021be8d23757dbcba65b0bbc34dde6f52e0fb
William Betts <will.betts@hotmail.co.uk>
2019-01-31 22:33:03 +0000

    Cut down fargate cpu/memory

--- infrastructure/terraform/components/application-cluster/main.tf
+++ infrastructure/terraform/components/application-cluster/main.tf
@@ -10,11 +10,11 @@ module "ecs" {
   source = "../../modules/ecs-service"
   service_name = "hello-world"
   desired_count = "3"
-  memory = "2048"
+  memory = "512"
   private_subnets = "${module.networking.private_subnets}"
   public_subnets = "${module.networking.public_subnets}"
   cluster_arn = "${aws_ecs_cluster.cluster.arn}"
-  cpu = "1024"
+  cpu = "256"
   vpc_id = "${module.networking.vpc_id}"
   public_lb = true
 }
--- infrastructure/terraform/modules/ecs-service/main.tf
+++ infrastructure/terraform/modules/ecs-service/main.tf
@@ -97,6 +97,26 @@ resource "aws_lb" "lb" {
   enable_deletion_protection = false
 }
 
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.service_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "${var.vpc_id}"
+  target_type = "ip"
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_lb_listener" "lb" {
+  load_balancer_arn = "${aws_lb.lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.tg.id}"
+    type             = "forward"
+  }
+}
+
 resource "aws_security_group" "lb_sg" {
   name        = "${var.service_name}-lb-sg"
   description = "Load balancer sg"
@@ -138,24 +158,4 @@ resource "aws_security_group_rule" "ecr" {
   protocol        = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.service_sg.id}"
-}
-
-resource "aws_lb_target_group" "tg" {
-  name        = "${var.service_name}-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = "${var.vpc_id}"
-  target_type = "ip"
-}
-
-# Redirect all traffic from the ALB to the target group
-resource "aws_lb_listener" "lb" {
-  load_balancer_arn = "${aws_lb.lb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = "${aws_lb_target_group.tg.id}"
-    type             = "forward"
-  }
 }

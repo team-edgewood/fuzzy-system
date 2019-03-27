173a0cff22d737a4c298c85ce30dd530a2e9c178
William Betts <will.betts@hotmail.co.uk>
2019-03-27 19:36:34 +0000

    Increase terraform version

--- infrastructure/terraform/components/image-repos/run-tf.sh
+++ infrastructure/terraform/components/image-repos/run-tf.sh
@@ -5,8 +5,8 @@ set -eux
 cd $(dirname $0)
 
 which terraform || {
-  wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-  unzip terraform_0.11.11_linux_amd64.zip
+  wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+  unzip terraform_0.11.13_linux_amd64.zip
   mv terraform /usr/local/bin
 }
 
